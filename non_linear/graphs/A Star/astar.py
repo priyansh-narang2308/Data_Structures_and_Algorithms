fcccfc094796cceefca63979329ac3736b38e872
priyansh-narang2308 <145008344+priyansh-narang2308@users.noreply.github.com>
2024-12-25 19:09:34 +0530

    updated the astar algo

--- non_linear/graphs/A Star/astar.py
+++ non_linear/graphs/A Star/astar.py
@@ -1,32 +1,33 @@
 # Defining the graph as a dictionary where each node has a list of connected nodes with weights
 graphNodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('C', 3), ('D', 2)],
-    'C': [('D', 1), ('E', 5)],
-    'D': [('C', 1), ('E', 8)],
-    'E': [('I', 5), ('J', 5)],
-    'F': [('G', 1), ('H', 7)],
-    'G': [('I', 3), ('K', 2)],
-    'H': [('I', 2), ('L', 4)],
-    'I': [('E', 5), ('J', 3), ('M', 2)],
-    'J': [('N', 6)],
-    'K': [('M', 4), ('O', 2)],
-    'L': [('M', 3), ('P', 5)],
-    'M': [('N', 1), ('Q', 4)],
-    'N': [('R', 6)],
-    'O': [('Q', 3), ('S', 4)],
-    'P': [('R', 2), ('T', 6)],
-    'Q': [('U', 5)],
-    'R': [('V', 7)],
-    'S': [('U', 4), ('W', 3)],
-    'T': [('V', 3), ('X', 5)],
-    'U': [('Y', 2)],
-    'V': [('Z', 4)],
-    'W': [('Y', 3)],
-    'X': [('Z', 6)],
-    'Y': [('Z', 1)],
-    'Z': []
+    "A": [("B", 6), ("F", 3)],
+    "B": [("C", 3), ("D", 2)],
+    "C": [("D", 1), ("E", 5)],
+    "D": [("C", 1), ("E", 8)],
+    "E": [("I", 5), ("J", 5)],
+    "F": [("G", 1), ("H", 7)],
+    "G": [("I", 3), ("K", 2)],
+    "H": [("I", 2), ("L", 4)],
+    "I": [("E", 5), ("J", 3), ("M", 2)],
+    "J": [("N", 6)],
+    "K": [("M", 4), ("O", 2)],
+    "L": [("M", 3), ("P", 5)],
+    "M": [("N", 1), ("Q", 4)],
+    "N": [("R", 6)],
+    "O": [("Q", 3), ("S", 4)],
+    "P": [("R", 2), ("T", 6)],
+    "Q": [("U", 5)],
+    "R": [("V", 7)],
+    "S": [("U", 4), ("W", 3)],
+    "T": [("V", 3), ("X", 5)],
+    "U": [("Y", 2)],
+    "V": [("Z", 4)],
+    "W": [("Y", 3)],
+    "X": [("Z", 6)],
+    "Y": [("Z", 1)],
+    "Z": [],
 }
+
 
 # Function to fetch the neighbors of a given node
 def get_neighbors(v):
@@ -35,15 +36,39 @@ def get_neighbors(v):
     else:
         return None
 
+
 # Heuristic function: This function has a value/cost assigned to each node. Usually this is desgined to calculate the cost of the route designed
 def heuristic(n):
     heuristic_distance = {
-        'A': 25, 'B': 24, 'C': 23, 'D': 22, 'E': 21, 'F': 20,
-        'G': 19, 'H': 18, 'I': 17, 'J': 16, 'K': 15, 'L': 14,
-        'M': 13, 'N': 12, 'O': 11, 'P': 10, 'Q': 9, 'R': 8,
-        'S': 7, 'T': 6, 'U': 5, 'V': 4, 'W': 3, 'X': 2, 'Y': 1, 'Z': 0
+        "A": 25,
+        "B": 24,
+        "C": 23,
+        "D": 22,
+        "E": 21,
+        "F": 20,
+        "G": 19,
+        "H": 18,
+        "I": 17,
+        "J": 16,
+        "K": 15,
+        "L": 14,
+        "M": 13,
+        "N": 12,
+        "O": 11,
+        "P": 10,
+        "Q": 9,
+        "R": 8,
+        "S": 7,
+        "T": 6,
+        "U": 5,
+        "V": 4,
+        "W": 3,
+        "X": 2,
+        "Y": 1,
+        "Z": 0,
     }
     return heuristic_distance[n]
+
 
 # Implementation of the A* Algorithm
 def aStarAlgorithm(start_node, stop_node):
@@ -53,9 +78,9 @@ def aStarAlgorithm(start_node, stop_node):
     closed_set = set()
     # Dictionary to store the cost to reach each node
     g = {}
-    # Dictionary to store the parent of each node for remaking the path 
+    # Dictionary to store the parent of each node for remaking the path
     parents = {}
-    #The staring node always has the staring cost to be 0
+    # The staring node always has the staring cost to be 0
     g[start_node] = 0
     # The parent of the start node is itself
     parents[start_node] = start_node
@@ -73,7 +98,7 @@ def aStarAlgorithm(start_node, stop_node):
             pass
         else:
             # Explore the neighbors of the current node
-            for (m, weight) in get_neighbors(n):
+            for m, weight in get_neighbors(n):
                 # If the neighbor is not in open or closed set, add it to the open set
                 if m not in open_set and m not in closed_set:
                     open_set.add(m)
@@ -90,7 +115,7 @@ def aStarAlgorithm(start_node, stop_node):
 
         # If no path exists, return an error message
         if n == None:
-            print('Path does not exist!')
+            print("Path does not exist!")
             return None
 
         # If the destination is reached, remake and return the path
@@ -101,7 +126,7 @@ def aStarAlgorithm(start_node, stop_node):
                 n = parents[n]
             path.append(start_node)
             path.reverse()
-            print('Path found: {}'.format(path))
+            print("Path found: {}".format(path))
             return path
 
         # Move the current node from open set to closed set
@@ -109,8 +134,9 @@ def aStarAlgorithm(start_node, stop_node):
         closed_set.add(n)
 
     # If the loop exits without finding a path, return an error message
-    print('Path does not exist!')
+    print("Path does not exist!")
     return None
+
 
 # Display available nodes
 print("\nNodes Available:")
@@ -123,4 +149,4 @@ point_a = input("Enter the Starting point: ")
 point_b = input("Enter the destination point: ")
 
 # Call the main algorithm function to find the shortest path of the given starting point and ending point
-aStarAlgorithm(point_a, point_b)
+aStarAlgorithm(point_a, point_b)
